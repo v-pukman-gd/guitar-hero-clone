195bd24c6d96c6a171d0632afa61e3d03ee7d42d
Victor Pukman <pukman.victor@gmail.com>
2019-06-01 23:16:13 +0500

    map added

--- game.gd
+++ game.gd
@@ -4,8 +4,9 @@ onready var music_node = $Music
 onready var road_node = $Road
 
 var audio
+var map
 var audio_file = "res://audio.ogg"
-var audio_file2 = "res://audio2.ogg"
+var map_file = "res://map.mboy"
 
 var tempo
 var bar_length_in_m
@@ -15,18 +16,27 @@ var note_scale
 var start_pos_in_sec
 
 func _ready():
-	audio = load(audio_file2)
+	audio = load(audio_file)
+	map = load_map()
 	calc_params()
 	
 	music_node.setup(self)
 	road_node.setup(self)
 	
 func calc_params():
-	tempo = 104
-	bar_length_in_m = 8 # Godot meters
+	tempo = int(map.tempo)
+	bar_length_in_m = 16 # Godot meters
 	quarter_time_in_sec = 60/float(tempo) # 60/60 = 1, 60/85 = 0.71
 	speed = bar_length_in_m/float(4*quarter_time_in_sec) # each bar has 4 quarters # 
 	note_scale = bar_length_in_m/float(4*400)
-	start_pos_in_sec = 0
+	start_pos_in_sec = (float(map.start_pos)/400.0) * quarter_time_in_sec
+	
+func load_map():
+	var file = File.new()
+	file.open(map_file, File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return JSON.parse(content).get_result()
+	
 	
 
--- road.gd
+++ road.gd
@@ -9,11 +9,16 @@ var curr_location
 var speed
 var note_scale
 
+var curr_bar_index
+var tracks_data 
+
 func setup(game):
 	speed = Vector3(0,0,game.speed)
 	bar_length_in_m = game.bar_length_in_m
 	curr_location = Vector3(0,0,-bar_length_in_m)
 	note_scale = game.note_scale
+	curr_bar_index = 0
+	tracks_data = game.map.tracks
 	add_bars()
 	
 		
@@ -26,13 +31,21 @@ func _process(delta):
 			add_bar()
 		
 
-func add_bar():
+func add_bar():	
 	var bar = bar_scn.instance()
 	bar.translation = Vector3(curr_location.x, curr_location.y, curr_location.z)
 	bar.note_scale = note_scale
+	bar.bar_data = get_bar_data()
 	bars.append(bar)
 	bars_node.add_child(bar)
 	curr_location += Vector3(0,0,-bar_length_in_m)
+	curr_bar_index += 1
+	
+func get_bar_data():
+	var left_data = tracks_data[0].bars[curr_bar_index]
+	var center_data = tracks_data[1].bars[curr_bar_index]
+	var right_data = tracks_data[2].bars[curr_bar_index]
+	return [left_data, center_data, right_data]
 	
 func remove_bar(bar):
 	bar.queue_free()
