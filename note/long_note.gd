3dd17b619cdab6e3e83ec74ca800466ac97e7fa1
Victor Pukman <pukman.victor@gmail.com>
2019-07-06 23:14:23 +0500

    fix capture pos

--- note/long_note.gd
+++ note/long_note.gd
@@ -5,6 +5,8 @@ var curr_length_in_m
 var hold_started = false
 var hold_canceled = false
 
+var captured = false
+
 func _on_ready():
 	._on_ready()
 	curr_length_in_m = max(100, length - 100)*length_scale
@@ -21,10 +23,16 @@ func _on_process(delta):
 				hold_canceled = true  # drop the note
 		
 		if hold_started and not hold_canceled:
-			curr_length_in_m -= speed.z * delta
-			if curr_length_in_m <= 0:
-				collect()
-			else:
-				$Beam.scale = Vector3(1,1,curr_length_in_m)
-				translate(-speed*delta)
+			if not captured:
+				var diff = picker.global_transform.origin.z - self.global_transform.origin.z
+				if abs(diff) <= 0.1:
+					captured = true
+					translate(Vector3(0,0,diff))
+			else:			
+				curr_length_in_m -= speed.z * delta
+				if curr_length_in_m <= 0:
+					collect()
+				else:
+					$Beam.scale = Vector3(1,1,curr_length_in_m)
+					translate(-speed*delta)
 			
